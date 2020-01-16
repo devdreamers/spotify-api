14d70e3539cfe1bb5518ee7003a977cdff14fd7f
znffl <jul0218@gmail.com>
2020-01-16 13:56:42 +0900

    add client id/pwd & mysql id/pwd

--- spotify_api_14.2.py
+++ spotify_api_14.2.py
@@ -1,5 +1,17 @@
 
+## spotify access info
+client_id = "55c44a45388048d88dd64df9ec727859"
+client_secret = "754c1097a62a4d3fb4949b36be5ac517"
+
+## DB연결정보
+host = "fastcampus.c7qmculsyi1n.ap-northeast-2.rds.amazonaws.com"
+port = 3306
+username = "sean"
+database = "production"
+password = "password1"
 
 
-
-## RDS 아이디
+## RDS 아티스트 ID를 가져오고
+## Spotify API 를 통해서 데이터를 불러오고
+## .json 
+## S3 import
